beaabf3d78ee8a41ff7c03853f436ad7f8f029b6
Pradipta Chatterjee <pradipta.chatterjee@globalids.com>
2023-11-08 20:36:33 +0530

    🖼️ image details page added

---
+++ pages/[id].vue
@@ -0,0 +1,21 @@
+<template>
+	<UContainer>
+		<UCard class="grid grid-cols-12 grid-rows-6">
+			<header class="row-span-2">
+				<Placeholder class="h-32"></Placeholder>
+			</header>
+
+			<div class="row-span-4">
+				<div class="col-span-10"></div>
+				<div class="col-span-2"></div>
+			</div>
+		</UCard>
+	</UContainer>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+const { state, fetchPhotoDetails } = useUnsplash();
+
+fetchPhotoDetails(route.params.id.toString());
+</script>
