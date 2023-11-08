0fc2f345f34c8507aaa7fc226d9e85f9dfb7add5
Pradipta Chatterjee <pradipta.chatterjee@globalids.com>
2023-11-08 20:35:06 +0530

    ⚙️ meta data added for app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@nuxt/ui", "@nuxt/image"],
+	app: {
+		head: {
+			charset: "utf-16",
+			title: "Unsplash Clone",
+			meta: [
+				{
+					name: "Unsplash Clone",
+					content:
+						"Application to search and download free stock royalty free images.",
+				},
+			],
+		},
+	},
 	runtimeConfig: {
 		public: {
 			ACCESS_KEY: import.meta.env.NUXT_ACCESS_KEY,
