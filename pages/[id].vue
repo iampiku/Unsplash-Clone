9c8f926b00a60e706ba5b1d4ed6a0bef1e815159
iampiku <iampikuchatterjee@gmail.com>
2023-11-10 10:32:22 +0530

    🔧 initial implementation of details page

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,13 +1,50 @@
 <template>
-	<UContainer>
-		<UCard class="grid grid-cols-12 grid-rows-6">
-			<header class="row-span-2">
-				<Placeholder class="h-32"></Placeholder>
-			</header>
+	<UContainer class="max-w-full p-6 my-4">
+		<UCard class="max-w-full">
+			<template #header>
+				<header class="flex justify-between">
+					<div class="flex items-center">
+						<UAvatar
+							size="lg"
+							class="mr-2"
+							:src="state.photoDetails?.user.profile_image.small"
+						></UAvatar>
 
-			<div class="row-span-4">
-				<div class="col-span-10"></div>
-				<div class="col-span-2"></div>
+						<div class="flex flex-col text-lg">
+							<span class="font-bold">{{ name }}</span>
+							<span class="font-semibold">{{ publishedOn }}</span>
+						</div>
+					</div>
+					<div class="flex items-center text-lg font-semibold">
+						<UIcon name="i-heroicons-heart"></UIcon>
+						<span class="ml-2">{{ state.photoDetails?.likes }}</span>
+					</div>
+				</header>
+			</template>
+			<div class="grid grid-cols-12">
+				<NuxtImg
+					format="webp"
+					loading="lazy"
+					class="rounded-lg col-span-4"
+					:alt="state.photoDetails?.id"
+					:src="state.photoDetails?.urls.full"
+				></NuxtImg>
+				<div id="stats" class="col-span-8">
+					<div class="flex justify-around text-2xl">
+						<div>
+							<p class="font-light">Views</p>
+							<p class="font-semibold">234,543</p>
+						</div>
+						<div>
+							<p class="font-light">Downloads</p>
+							<p class="font-semibold">234,543</p>
+						</div>
+						<div>
+							<p class="font-light">Published On</p>
+							<p class="font-semibold">234,543</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</UCard>
 	</UContainer>
@@ -16,6 +53,19 @@
 <script lang="ts" setup>
 const route = useRoute();
 const { state, fetchPhotoDetails } = useUnsplash();
-
 fetchPhotoDetails(route.params.id.toString());
+
+const name = computed(() => {
+	return `${state.photoDetails?.user.first_name} ${state.photoDetails?.user.last_name}`;
+});
+
+const publishedOn = computed(() => {
+	const originalData = new Date(state.photoDetails?.created_at ?? "");
+
+	return originalData.toLocaleString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	});
+});
 </script>
